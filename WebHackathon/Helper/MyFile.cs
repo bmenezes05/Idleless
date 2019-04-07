f20c48818a6870d989e23b3ba297a606763e101e
ascarelli <allanscarelli@gmail.com>
2019-04-06 22:06:00 -0300

    add get agendamentos

---
+++ WebHackathon/Helper/MyFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace WebHackathon.Helper
+{
+    public static class MyFile
+    {
+
+        public static void saveJson(string json)
+        {
+            try
+            {
+                string path = System.Web.Hosting.HostingEnvironment.MapPath("~/Json/");
+                FileStream fs = new FileStream(path + "events.json", FileMode.Append);
+                StreamWriter sw = new StreamWriter(fs);
+                sw.WriteLine(json);
+                sw.Flush();
+                sw.Close();
+                fs.Close();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+    }
+}
